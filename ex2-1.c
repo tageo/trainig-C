fb9f8e0f399089cd157cc424b747ef659f5dee58
owani <ttaaggeeoo@gmail.com>
2023-12-13 21:52:52 +0900

    ex2-1

---
+++ ex2-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("a = ");
+    scanf("%d", &a);
+    printf("b = ");
+    scanf("%d", &b);
+    printf("a + b = %d\n",a+b);
+    printf("a - b = %d\n",a-b);
+    printf("a * b = %d\n",a*b);
+    printf("a / b = %d\n",a/b);
+    printf("a %% b = %d\n",a%b);
+}
