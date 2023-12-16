466eacc917482176bc070ea20a76c4e6905e6375
owani <ttaaggeeoo@gmail.com>
2023-12-16 11:26:54 +0900

    ex4-18

---
+++ ex4-18.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j,k;
+    k = 10;
+    
+    for(i = 0; i < k; i++)
+    {
+        for(j = k; j > 0; j--)
+        {
+            if( (k - j) == i){
+                printf("□");
+            }
+            else{
+                printf("■");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
